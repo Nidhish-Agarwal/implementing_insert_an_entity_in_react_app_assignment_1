d8816067fb8c2f17a82c8c029fdecde3b74e1be4
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2025-01-28 12:15:56 +0530

    Completed the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 // src/App.jsx
 
-import React from 'react';
-import Home from './Home';
-import './App.css'; // Import global styles
+import React from "react";
+import Home from "./Home";
+import "./App.css"; // Import global styles
+import { Routes, Route } from "react-router-dom";
+import AddBook from "./AddBook";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/add-book" element={<AddBook />}></Route>
+      </Routes>
     </div>
   );
 }
